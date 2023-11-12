aff3111337827d87600fee6ea4a6772699189144
Coding-Scorpion <raheemzada385@gmail.com>
2023-11-12 00:10:54 -0800

    initial commit

---
+++ day1.js
@@ -0,0 +1,23 @@
+// console.log("hellow world")
+// let name = "My name is Raheem Zada".
+// let education = "I am final year student of Bs Software Engineering at Brains Institute Peshawar."
+// let age = "I am 22 Years old."
+
+// console.log(name)
+
+
+let name = "Raheem Xada"
+let qaulification = "BS"
+let university = "Brains University"
+let birth_place = "charsadda"
+let age_in_years = 22
+let profession = "Student"
+let weight_in_kg = 58
+let height_in_feet = 5.5
+let income_in_dollars = 100
+let birth_year = 2023- age_in_years
+let weight_in_lb = weight_in_kg * 2.2
+let height_in_inches = height_in_feet * 12
+let income_in_rupees = income_in_dollars * 282
+
+console.log
